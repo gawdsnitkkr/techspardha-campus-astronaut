11e5560a356c6c504ce6b6b113a305396c0929d4
rishabh2609 <rishabhchanana97@gmail.com>
2016-11-20 17:17:51 +0530

    Thank you page

--- js/thank.js
+++ js/thank.js
@@ -83,35 +83,3 @@
 		setTimeout(Functions.callInterval(), 10000);
 	});
 })(document, jQuery(document), jQuery);
-function postToGoogle() {
-			var name = $('#Name').val();
-        	var email = $('#Email').val();
-        	var college = $('#College').val();
-			var mno = $('#Mno').val();
-			var year = $('#Year').val();
-			var cmt1 = $('#cmt1').val();
-			var cmt2 = $('#cmt2').val();
-			var cmt3 = $('#cmt3').val();
-         	$.ajax({
-                	url: "https://docs.google.com/forms/d/e/1FAIpQLSe8qPXKvTOCxrsfLgqwGYjda0TckC66eN8-8VcU4HhdVa3nIg/formResponse",
-					data: {"entry.2094364885": name, 
-							"entry.2003932882": email, 
-							"entry.609785890": college, 
-							"entry.1438636419": mno, 
-							"entry.1320656763": year, 
-							"entry.1565698868": cmt1, 
-							"entry.902102950": cmt2,
-							"entry.1392460848": cmt3
-						},
-                	type: "POST",
-                	dataType: "xml",
-          			statusCode: {
-                        0: function() {
-                            window.location.replace("thankYou.html");
-                        },
-                        200: function() {
-                            window.location.replace("thankYou.html");
-                        }
-                    }
-            });
-}
